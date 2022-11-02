1b9671f5f22e634460ba4d6932efea73cbf9f0fa
Iftykhar-alam <47248273+iftykhar@users.noreply.github.com>
2022-11-02 11:29:13 +0600

    variable and uses of variable convensions

---
+++ php01/php01.php
@@ -0,0 +1,18 @@
+<?php
+
+echo 'hello php';
+
+echo "\n";
+
+// working on variables
+$numb = 45; //integer type variable
+
+$age = 'numb';
+
+$name = 'Iftykhar';
+echo $$age."\n";
+
+echo "Hi there, i am{$name}\nand i am {$$age}";
+
+
+?>

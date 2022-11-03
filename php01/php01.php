bb4be2db6bbfd5b476939a36ac14933b42b8359a
Iftykhar-alam <47248273+iftykhar@users.noreply.github.com>
2022-11-03 12:08:12 +0600

    constat

--- php01/php01.php
+++ php01/php01.php
@@ -1,18 +1,29 @@
 <?php
 
-echo 'hello php';
-
-echo "\n";
-
 // working on variables
 $numb = 45; //integer type variable
 
-$age = 'numb';
+$age = 'numb'; //string type
 
 $name = 'Iftykhar';
-echo $$age."\n";
+echo $$age."\n"; // the $$age is $($age) where $age = 'numb' and $numb = 45  so $$age is that and (.) is the concatenation tag for PHP
 
 echo "Hi there, i am{$name}\nand i am {$$age}";
+
+
+// discussing CONSTANTS 
+
+//Generally constants are written at the beggining portion of the code but here for learning purpose we are doing it on later lines of the code
+
+define('PI',3.1415); /* constants are declared with DEFINE keyword and unchangeable by later lines of code but
+                         prior or previous lines of code can change  the later line codes excution */ 
+
+$radius = 8;
+
+$area = 2*$radius*PI;
+
+echo "\nThe circumfarence of the wheel is \nArea ={$area}";
+
 
 
 ?>

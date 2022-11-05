e3d7c2289ac6ac80f129807bd56414bfa194f5a2
Iftykhar-alam <47248273+iftykhar@users.noreply.github.com>
2022-11-05 09:19:35 +0600

    Arithmatic And Assignment Operator

---
+++ php01/php01_op.php
@@ -0,0 +1,15 @@
+<?php
+
+$number = 12; //assigned value
+// $number += 12; //addtion operator
+// $number -= 6; // Subtraction Operator
+// $number /= 3; //Division Oparetor
+// $number **= 2; // Power Oparetor
+$number3 = $number % 10; // Modulas(reminder) Oparetor
+
+$number2 = $number + 12;
+
+
+
+echo $number,"\n".$number2,"\n".$number3;
+

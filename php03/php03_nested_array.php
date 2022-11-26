516132bd297acca1a8bfdf81408b2c4dd716dc6f
Iftykhar-alam <47248273+iftykhar@users.noreply.github.com>
2022-11-26 20:36:46 +0600

    nested or multi-dimension array

--- php03/php03_nested_array.php
+++ php03/php03_nested_array.php
@@ -13,4 +13,24 @@ $foods = [
 
 array_push($foods['fruits'],'jack fruit');
 // var_dump($foods);
-print_r($foods);
+// print_r($foods);
+echo "====---=====".PHP_EOL;
+
+// echo $foods['vegitables'][3];
+
+
+$sample = [
+    'test'=>[
+        'test1'=>['nested array'],
+    ],
+];
+
+echo $sample['test']['test1'][0];
+echo "====---=====".PHP_EOL;
+
+$sample2 = [
+    [1,2,3,4],
+    [11,22,33,44],
+    [111,222,333,444],
+];
+echo $sample2[2][2];

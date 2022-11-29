5202b8b6b089171fa207f7d4bf82d3504c66112e
Iftykhar-alam <47248273+iftykhar@users.noreply.github.com>
2022-11-29 11:02:38 +0600

    array to string conv serialize and JSON

--- php03/php03_assoc.php
+++ php03/php03_assoc.php
@@ -90,3 +90,44 @@ for($i = 0; $i < count($values); $i++){
     // echo $drinks[$value].PHP_EOL;
     echo $value.PHP_EOL;
 }
+
+echo "*******--------=========-------******".PHP_EOL;
+$student = array(
+    'fname'=>'kamal',
+    'lname'=>'ahmed',
+    'age'=>'20',
+    'class'=>9,
+    'section'=>'B',
+);
+
+print_r($student);
+
+// echo $student['age'];
+echo $student['fname']." ".$student['lname']."\n";
+
+printf("%s %s \n", $student['fname'],$student['lname']);
+
+echo join($student); //join is used for turning array into string
+echo "*******--------=========-------******".PHP_EOL;
+
+$stus =  serialize($student); // serialize() is a function  to convert array into  string  change array into a string 
+
+echo "\n";
+// $stu = unserialize($student);
+echo $stus;
+
+$newstus = unserialize($stus);  // unserialize() is a function to revert any serialized string into array once again but not all of it is readable through human eye PHP can read it just fine
+
+
+print_r($newstus); 
+
+echo PHP_EOL;
+
+#JSON JavaScript Object Notation is used to convert array into string as well it is readable entirly by humans and most importantly it is useable is JavaScript 
+
+$jsondata = json_encode($student); //json_encode() is a function that is used to turn any array into json format 
+echo $jsondata;
+// $student02 = json_decode($jsondata); //json_decode() is the fucntion that  reverts the json format to previous form. or more likely to standerd formate.
+$student02 = json_decode($jsondata, true);  //but to find it  the decoded data into array formate you must add the json_decode(json, true) parameter "true" additively 
+// echo $student02;
+print_r($student02);
